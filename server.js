8fce46ae1d487bbe6abc4bcbef0183852a116c2e
Rathik Pratap <52971518+rathikpratap@users.noreply.github.com>
2024-02-07 17:06:18 +0530

    cors commit

--- server.js
+++ server.js
@@ -6,7 +6,9 @@ const cors = require('cors');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: 'http://login.admixmedia.in'
+}));
 
 require('./config');
 
