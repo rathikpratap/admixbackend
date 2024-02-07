64197c7364e1bbba90427117c8af41625c6611d7
Rathik Pratap <52971518+rathikpratap@users.noreply.github.com>
2024-02-07 18:36:13 +0530

    commit

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors({
-    origin: 'http://login.admixmedia.in',
+    origin: 'http://localhost:5000',
     credentials:true,
 }));
 
