109af541a4a00537b258a7d4f315acad03c609e8
Rathik Pratap <52971518+rathikpratap@users.noreply.github.com>
2024-12-07 13:39:29 +0530

    fb token  Commit

--- auth-route.js
+++ auth-route.js
@@ -2215,7 +2215,7 @@ router.get('/salesSecondFacebook-leads', async (req, res) => {
     //Local
     //const response = await axios.get(`https://graph.facebook.com/v19.0/me?fields=adaccounts%7Bid%2Ccampaigns%7Bid%2Cname%2Cads%7Bname%2Cleads%7D%7D%7D&access_token=${accessToken}`);
     //Real
-    const accessToken2 = 'EAAHV6LHxdvoB09sZBB30NjJfOcTGMPY8ZCk0ZBhRQ9DOtt2xPQA6ZCzksdE2OgxqH7PaPLr3x6WOjT4ff2ZAKzUBnlNQCm2a4Q6rvPqZBZAr6aCGpOilEDR1OD3J9cYXTu9i1bYGsT7y8OwiWBfcP2JZCQ8KNK7HMzwqj60tn9dINC778RFigUGZBeBxM';
+    const accessToken2 = 'EAAHV6LHxdvoBOZBtIpBHr85vL1gk8u3NDVTFMiHkEAt8I7oRpTNBlluORRxTGyiTEunIHFihyYFy4r6ZASF7HaYDjElAaYGU5cVdfi0ZBYxKoCJYNBP7lQqWo4i0pwnOo4AnbHDcMDhDj8r9lo37iUg9rImZC4oJJSo733RdtqZAcvkeCwqzmdGnl';
     const response = await axios.get(`https://graph.facebook.com/v19.0/me?fields=id%2Cadaccounts%7Bcampaigns%7Bid%2Cname%2Cads%7Bname%2Cleads%7D%7D%7D&access_token=${accessToken2}`);
     const leadsData = response.data.adaccounts.data;
     let cust_name, company_name, phone, state, email = '';
