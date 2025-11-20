b8c92be325d641ad747250a4bad8ad99389258c6
Rathik Pratap <52971518+rathikpratap@users.noreply.github.com>
2025-11-20 11:02:16 +0530

    contact Commit

--- auth-route.js
+++ auth-route.js
@@ -2780,7 +2780,7 @@ router.get('/facebook-leads', async (req, res) => {
 const CLIENT_ID = '163851234056-46n5etsovm4emjmthe5kb6ttmvomt4mt.apps.googleusercontent.com';
 const CLIENT_SECRET = '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';
 const REDIRECT_URI = 'https://developers.google.com/oauthplayground';
-const REFERESH_TOKEN = '1//04pAJ5zxMP1VPCgYIARAAGAQSNwF-L9IrIlSgUNYQ_Eed96P2hWXI_w6dt7AFYwMPltLKiCWvLPWx81yUTPI57GgFkaXQvfsSaNA';
+const REFERESH_TOKEN = '1//04LJCaz4SWDgrCgYIARAAGAQSNwF-L9Ir5E4Vfg2qESWU8Bg65XKh_8klh9HAnDXVhAoUicYmlgMCtmLcIVgQnPvO6HzDYawGVbg';
 
 const oauth2Client = new google.auth.OAuth2(
   CLIENT_ID,
